e2d93ffd7e44dd933f0827ea858531c2e89e0ef5
jswelsh <james.sinclair.welsh@gmail.com>
2020-09-14 16:28:11 -0700

    create constructer class

--- src/component/converter/converter.js
+++ src/component/converter/converter.js
@@ -3,3 +3,16 @@ import axios from "axios";
 import "./converter.css";
 
 export default Converter;
+
+class Converter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      fromCurrency: "USD",
+      toCurrency: "GBP",
+      amount: 1,
+      currencies: []
+    };
+  }
+}
