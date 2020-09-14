3322200f4e5d757f4f7220b827b05ca1b381e0ac
jswelsh <james.sinclair.welsh@gmail.com>
2020-09-14 16:21:06 -0700

    structure app hierarchy

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Converter from "./component/converter/converter";
+
+class App extends Component {
+  render () {
+    return <Converter />;
+  }
+}
+export default App;
