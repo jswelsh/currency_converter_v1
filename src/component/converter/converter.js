2bc8dcc78d38532087b5f70e83688ceeb3204702
jswelsh <james.sinclair.welsh@gmail.com>
2020-09-14 16:38:15 -0700

    add did mount func, need to replace later and remove class style

--- src/component/converter/converter.js
+++ src/component/converter/converter.js
@@ -6,7 +6,7 @@ export default Converter;
 
 class Converter extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); //need to update this as its depreciated
     this.state = {
       result: null,
       fromCurrency: "USD",
@@ -15,4 +15,19 @@ class Converter extends React.Component {
       currencies: []
     };
   }
+  componentDidMount() {
+    axios
+      .get("https://api.openrates.io/latest")
+      .then(res => {
+        const currencyArr = ["EUR"];
+        for (const key in response.data.rates) {
+          currencyArr.push(key);
+        }
+        this.setState({currencies: currencyArr});
+      })
+      .catch(err => {
+        console.log("Something went wrong", err);
+      });
+  }
+  
 }
