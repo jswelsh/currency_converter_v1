c84f680496ca47c4c528b5567d138731a2c90789
jswelsh <james.sinclair.welsh@gmail.com>
2020-09-18 02:21:58 -0700

    create wire frame for GET request for historical data, need to get basic functionality b4 moving to sept hook file... maybe need to also change dispatcher and make a func controller for switching between the two graphs?.?>?

--- src/components/CurrencyHistoryGraph.js
+++ src/components/CurrencyHistoryGraph.js
@@ -5,11 +5,108 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 am4core.useTheme(am4themes_animated);
 
+const getCurrencies = axios.get("https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01&base=USD HTTP/1.1")
+
+
+/* 
+
+const convertHandler = () => {
+    if (state.fromCurrency !== state.toCurrency) {  
+      axios
+        .get(
+          `https://api.openrates.io/latest?base=${
+            state.fromCurrency}&symbols=${state.toCurrency}`
+        )
+        .then(res => {
+          const result = 
+          state.amount * res.data.rates[state.toCurrency];
+          setResult(result.toFixed(5));
+        })
+        .catch(error => {
+          console.log("Opps", error.message);
+        });
+    } else {
+      setResult("You cant convert the same currency!");
+    }
+  };
+
+*/
 
 export default function CurrencyConversionGraph() {
   let chart = am4core.create("chartdiv", am4charts.XYChart);
 
-  chart.data = [{
+  chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+
+  let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  
+/*  valueAxis.renderer.minWidth = 50;
+  valueAxis.min = 0;
+  valueAxis.cursorTooltipEnabled = false; */
+
+  let series = chart.series.push(new am4charts.LineSeries());
+  series.dataFields.valueY = "value";
+  series.dataFields.dateX = "date";
+  series.tooltipText = "{value}";
+  //series.tooltip.pointerOrientation = "vertical";
+  series.strokeWidth = 2;
+  series.minBulletDistance = 15;
+
+  // Drop-shaped tooltips
+  series.tooltip.background.cornerRadius = 20;
+  series.tooltip.background.strokeOpacity = 0;
+  series.tooltip.pointerOrientation = "vertical";
+  series.tooltip.label.minWidth = 40;
+  series.tooltip.label.minHeight = 40;
+  series.tooltip.label.textAlign = "middle";
+  series.tooltip.label.textValign = "middle";
+
+
+  // Make bullets grow on hover
+  let bullet = series.bullets.push(new am4charts.CircleBullet());
+  bullet.circle.strokeWidth = 2;
+  bullet.circle.radius = 4;
+  bullet.circle.fill = am4core.color("#fff");
+
+  let bullethover = bullet.states.create("hover");
+  bullethover.properties.scale = 1.3;
+
+  // Make a panning cursor
+  chart.cursor = new am4charts.XYCursor();
+  chart.cursor.behavior = "panXY";
+  chart.cursor.xAxis = dateAxis;
+  chart.cursor.snapToSeries = series;
+
+  // Create vertical scrollbar and place it before the value axis
+  chart.scrollbarY = new am4core.Scrollbar();
+  chart.scrollbarY.parent = chart.leftAxesContainer;
+  chart.scrollbarY.toBack();
+
+  // Create a horizontal scrollbar with previe and place it underneath the date axis
+  chart.scrollbarX = new am4charts.XYChartScrollbar();
+  chart.scrollbarX.series.push(series);
+  chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+  dateAxis.start = 0.79;
+  dateAxis.keepSelection = true;
+
+
+  /* chart.scrollbarX = new am4charts.XYChartScrollbar();
+  chart.scrollbarX.series.push(series);
+   */
+/*   chart.cursor = new am4charts.XYCursor();
+  chart.cursor.xAxis = categoryAxis;
+  chart.cursor.snapToSeries = series; */
+
+
+  return (
+    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+  );
+}
+
+
+/* 
+ chart.data = [{
     "date": "2012-07-27",
     "value": 13
   }, {
@@ -573,76 +670,4 @@ export default function CurrencyConversionGraph() {
   }, {
     "date": "2013-01-30",
     "value": 81
-  }]
-
-
-  chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
-
-  let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-  
- /*  valueAxis.renderer.minWidth = 50;
-  valueAxis.min = 0;
-  valueAxis.cursorTooltipEnabled = false; */
-
-  let series = chart.series.push(new am4charts.LineSeries());
-  series.dataFields.valueY = "value";
-  series.dataFields.dateX = "date";
-  series.tooltipText = "{value}";
-  //series.tooltip.pointerOrientation = "vertical";
-  series.strokeWidth = 2;
-  series.minBulletDistance = 15;
-
-  // Drop-shaped tooltips
-  series.tooltip.background.cornerRadius = 20;
-  series.tooltip.background.strokeOpacity = 0;
-  series.tooltip.pointerOrientation = "vertical";
-  series.tooltip.label.minWidth = 40;
-  series.tooltip.label.minHeight = 40;
-  series.tooltip.label.textAlign = "middle";
-  series.tooltip.label.textValign = "middle";
-
-
-  // Make bullets grow on hover
-  let bullet = series.bullets.push(new am4charts.CircleBullet());
-  bullet.circle.strokeWidth = 2;
-  bullet.circle.radius = 4;
-  bullet.circle.fill = am4core.color("#fff");
-
-  let bullethover = bullet.states.create("hover");
-  bullethover.properties.scale = 1.3;
-
-  // Make a panning cursor
-  chart.cursor = new am4charts.XYCursor();
-  chart.cursor.behavior = "panXY";
-  chart.cursor.xAxis = dateAxis;
-  chart.cursor.snapToSeries = series;
-
-  // Create vertical scrollbar and place it before the value axis
-  chart.scrollbarY = new am4core.Scrollbar();
-  chart.scrollbarY.parent = chart.leftAxesContainer;
-  chart.scrollbarY.toBack();
-
-  // Create a horizontal scrollbar with previe and place it underneath the date axis
-  chart.scrollbarX = new am4charts.XYChartScrollbar();
-  chart.scrollbarX.series.push(series);
-  chart.scrollbarX.parent = chart.bottomAxesContainer;
-
-  dateAxis.start = 0.79;
-  dateAxis.keepSelection = true;
-
-
-  /* chart.scrollbarX = new am4charts.XYChartScrollbar();
-  chart.scrollbarX.series.push(series);
-   */
-/*   chart.cursor = new am4charts.XYCursor();
-  chart.cursor.xAxis = categoryAxis;
-  chart.cursor.snapToSeries = series; */
-
-
-  return (
-    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-  );
-}
-
-
+  }] */
