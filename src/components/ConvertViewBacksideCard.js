708da3d387090154ac894a3a5fd7c3dd3c46137e
jswelsh <james.sinclair.welsh@gmail.com>
2020-11-12 09:06:54 -0800

    add the icon to the backside for the convert cards

--- src/components/ConvertViewBacksideCard.js
+++ src/components/ConvertViewBacksideCard.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { ConvertViewCardTable } from './ConvertViewCardTable'
+import { makeStyles } from '@material-ui/core/styles';
+
 import {
 	Card,
 	Grid,
 	CardHeader,
 	CardContent,
+	IconButton,
+	Typography
 } from '@material-ui/core';
+import ThreeDRotationIcon from '@material-ui/icons/ThreeDRotation';
+
+const useStyles = makeStyles((theme) => ({
+	icon:{
+		marginTop: 15,
+		marginBottom:15,
+		marginLeft: 10
+	}
+}));
 
 export function ConvertViewBacksideCard({
 	data,
@@ -13,8 +26,9 @@ export function ConvertViewBacksideCard({
 	recentRateHistory,
 	avatar
 }){
+const classes = useStyles();
 
-	console.log(recentRateHistory)
+
 return(
 <Card >
 	<CardHeader
@@ -22,7 +36,15 @@ return(
 		titleTypographyProps={{ align: 'left',variant: "h4" }}
 		subheader= {currency}
 		subheaderTypographyProps={{ align: 'left'}}
-		avatar={avatar}/>
+		avatar={avatar}
+		action={
+			<IconButton aria-label="settings" >
+				<Typography variant="h4" color="secondary"className={classes.icon}>
+					Flip 
+				</Typography>
+				<ThreeDRotationIcon fontSize='large'className={classes.icon}/>
+			</IconButton>
+		}/>
 	<CardContent>
 		<Grid container justify="center">
 			<Grid item >
